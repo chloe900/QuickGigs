1fdb22c85a35577d65605f353ade3332fe8a5874
chloe900 <chloetdube@icloud.com>
2024-07-24 22:05:25 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'signup/signup.dart';
+import 'signup/signin.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Landing Page Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LandingPage(), // Set the landing page as the home screen
+    );
+  }
+}
+
+// Main landing page
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: Text('QuickGigs', style: TextStyle(color: Colors.white)),
+        actions: [
+          TextButton(
+            onPressed: () {
+              //actions
+            },
+            child: Text('Home', style: TextStyle(color: Colors.white)),
+          ),
+          TextButton(
+            onPressed: () {
+              //actions
+            },
+            child: Text('How it works', style: TextStyle(color: Colors.white)),
+          ),
+           TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SignInPage()),
+              );
+            },
+            child: Text('Login', style: TextStyle(color: Colors.white)),
+          ),
+          TextButton(
+            onPressed: () {
+              // Handle navigation or other actions
+            },
+            child: Text('Contact', style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Section(
+              title: 'How It Works',
+              color: const Color.fromRGBO(255, 249, 196, 1),
+              content: 'Learn how our app works and get started quickly.',
+            ),
+          ),
+          Expanded(
+            child: Section(
+              title: 'Why Choose Us',
+              color: Colors.lightGreen,
+              content: 'Discover the benefits of using our app and why we stand out.',
+            ),
+          ),
+          Expanded(
+            child: Section(
+              title: 'Get Started',
+              color: Color.fromARGB(255, 228, 177, 159),
+              content: 'Sign up now and begin your journey with us.',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Section extends StatelessWidget {
+  final String title;
+  final Color color;
+  final String content;
+
+  Section({
+    required this.title,
+    required this.color,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      width: double.infinity, // Ensures the container fills the horizontal space
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Makes text and content stretch to fill horizontal space
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center, // Centers text horizontally
+          ),
+          SizedBox(height: 10),
+          Text(
+            content,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center, // Centers text horizontally
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/signup/signup.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:quick_gigs/signup/signin.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _surnameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _signUp() {
+    // Add sign-up logic here
+    String name = _nameController.text;
+    String surname = _surnameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    // Authenticate and create account
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen[50], // Background color of the page
+      body: Center(
+        child: Container(
+          width: 350, // Width of the container
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white, // Background color of the container
+            borderRadius: BorderRadius.circular(12), // Rounded corners
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), // Shadow position
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max, // Minimize the column size
+            mainAxisAlignment: MainAxisAlignment.center, // Center contents vertically
+            children: [
+              Text("Create an account", style: TextStyle(color: Colors.brown, fontSize: 30 ),),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.brown, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.green, width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _surnameController,
+                decoration: InputDecoration(
+                  labelText: 'Surname',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.brown, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.green, width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.brown, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.green, width: 1),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.brown, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                    borderSide: BorderSide(color: Colors.green, width: 1),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+            Row(children: [
+               Expanded(
+              child:  ElevatedButton(
+                onPressed: _signUp,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lightGreen.withOpacity(0.9), // Background color of the button
+                  foregroundColor: Colors.brown, // Text color
+                ),
+                child: Text('Create Account'),
+              )
+             
+             )
+            ],),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignInPage()),
+                  );
+                },
+                style: TextButton.styleFrom(
+                          foregroundColor: Colors.brown,
+                          ),
+                child: Text('Already have an account? Sign in'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
