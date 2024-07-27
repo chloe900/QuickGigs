29be39341b378839cc1afbd2d6db7285003db3c7
chloe900 <chloetdube@icloud.com>
2024-07-27 13:03:24 +0200

    add job button

--- lib/listing.dart
+++ lib/listing.dart
@@ -2,6 +2,9 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'colours.dart'; 
 
+
+import 'package:flutter/material.dart';
+
 class ListingPage extends StatefulWidget {
   @override
   _ListingPageState createState() => _ListingPageState();
@@ -10,371 +13,188 @@ class ListingPage extends StatefulWidget {
 class _ListingPageState extends State<ListingPage> {
   final TextEditingController _searchController = TextEditingController();
 
+  void _showSubcategories(String title) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(3, (index) => GestureDetector(
+              onTap: () {
+                Navigator.pop(context); // Close the modal
+                // Add your selection handling logic here
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text('$title ${index + 1}', style: TextStyle(fontSize: 18)),
+              ),
+            )),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.brown,
         title: Text('QuickGigs', style: TextStyle(color: Colors.white)),
-         actions: <Widget>[
-    IconButton(
-      icon: Icon(
-        Icons.person,
-        color: Colors.white,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person, color: Colors.white),
+            onPressed: () {
+              // Handle user profile action
+            },
+          ),
+        ],
       ),
-      onPressed: () {
-        // do something
-      },
-    )
-  ],      
-      ),
-      body: FractionallySizedBox(
-        widthFactor: 1.0,
-        heightFactor: 1.0,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 4,
-              child: Container(
-                color: AppColors.yellow,
-                child: Center(child: Text('', style: TextStyle(color: Colors.white))),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: AppColors.yellow,
+              child: Center(child: Text('Some Content', style: TextStyle(color: Colors.white))),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.green[200],
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      textAlignVertical: TextAlignVertical.center,
+                      textAlign: TextAlign.left,
+                      controller: _searchController,
+                      style: const TextStyle(color: Colors.white),
+                      cursorColor: Colors.brown,
+                      decoration: InputDecoration(
+                        hintText: 'Search...',
+                        hintStyle: TextStyle(color: Colors.brown),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.brown, width: 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                  _buildFilterButton('Location'),
+                  _buildFilterButton('Category'),
+                  _buildFilterButton('Availability'),
+                  _buildFilterButton('Type'),
+                ],
               ),
             ),
-            Expanded(
-  flex: 1,
-  child: Container(
-    color: Colors.green[200],
-    child: Column(
-      children: [
-         Padding(
-          padding: const EdgeInsets.all(16.0), // Add padding to position the TextField
-          child: TextField(
-            textAlignVertical: TextAlignVertical.center,
-            textAlign: TextAlign.left,
-            controller: _searchController,
-            style: const TextStyle(color: Colors.white),
-            cursorColor: Colors.brown,
-            decoration: InputDecoration(
-              hintText: 'Search...',
-              hintStyle: TextStyle(color: Colors.brown),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12), // Rounded corners
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text('Add a Job', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Location',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Category',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Availabiltiy',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Type',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Submit'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: Text('Add Job', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+
+  Widget _buildFilterButton(String label) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GestureDetector(
+        onTap: () => _showSubcategories(label),
+        child: SizedBox(
+          width: double.infinity,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Colors.green),
+            ),
+            child: Center(
+              child: Text(
+                label,
+                style: TextStyle(color: Colors.white),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12), // Rounded corners
-                borderSide: BorderSide(color: Colors.green, width: 2),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12), // Rounded corners
-                borderSide: BorderSide(color: Colors.brown, width: 1),
-              ),
             ),
           ),
-         ),
-       Padding(
-  padding: const EdgeInsets.all(16.0), // Add padding to position the clickable text
-  child: GestureDetector(
-    onTap: () {
-      // Handle the click to show subcategories
-      showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 1 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 1', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 2 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 2', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 3 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 3', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                // Add more subcategories as needed
-              ],
-            ),
-          );
-        },
-      );
-    },
-    child: SizedBox(
-      width: double.infinity, // Make the widget fill the horizontal space
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-        decoration: BoxDecoration(
-          color: Colors.green, // Background color
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-          border: Border.all(color: Colors.green), // Match the border color
-        ),
-        child: Center(
-          child: Text(
-            'Location',
-            style: TextStyle(color: Colors.white), // Text color
-          ),
-        ),
-      ),
-    ),
-  ),
-),
-
- Padding(
-  padding: const EdgeInsets.all(16.0), // Add padding to position the clickable text
-  child: GestureDetector(
-    onTap: () {
-      // Handle the click to show subcategories
-      showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 1 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 1', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 2 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 2', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 3 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 3', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                // Add more subcategories as needed
-              ],
-            ),
-          );
-        },
-      );
-    },
-    child: SizedBox(
-      width: double.infinity, // Make the widget fill the horizontal space
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-        decoration: BoxDecoration(
-          color: Colors.green, // Background color
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-          border: Border.all(color: Colors.green), // Match the border color
-        ),
-        child: Center(
-          child: Text(
-            'Category',
-            style: TextStyle(color: Colors.white), // Text color
-          ),
-        ),
-      ),
-    ),
-  ),
-),
-Padding(
-  padding: const EdgeInsets.all(16.0), // Add padding to position the clickable text
-  child: GestureDetector(
-    onTap: () {
-      // Handle the click to show subcategories
-      showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 1 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 1', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 2 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 2', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 3 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 3', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                // Add more subcategories as needed
-              ],
-            ),
-          );
-        },
-      );
-    },
-    child: SizedBox(
-      width: double.infinity, // Make the widget fill the horizontal space
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-        decoration: BoxDecoration(
-          color: Colors.green, // Background color
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-          border: Border.all(color: Colors.green), // Match the border color
-        ),
-        child: Center(
-          child: Text(
-            'Availability',
-            style: TextStyle(color: Colors.white), // Text color
-          ),
-        ),
-      ),
-    ),
-  ),
-),
-Padding(
-  padding: const EdgeInsets.all(16.0), // Add padding to position the clickable text
-  child: GestureDetector(
-    onTap: () {
-      // Handle the click to show subcategories
-      showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 1 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 1', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 2 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 2', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle subcategory 3 selection
-                    Navigator.pop(context); // Close the modal
-                    // Add your selection handling logic here
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text('Subcategory 3', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-                // Add more subcategories as needed
-              ],
-            ),
-          );
-        },
-      );
-    },
-    child: SizedBox(
-      width: double.infinity, // Make the widget fill the horizontal space
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-        decoration: BoxDecoration(
-          color: Colors.green, // Background color
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-          border: Border.all(color: Colors.green), // Match the border color
-        ),
-        child: Center(
-          child: Text(
-            'Type',
-            style: TextStyle(color: Colors.white), // Text color
-          ),
-        ),
-      ),
-    ),
-  ),
-),
-
-
-
-
-
-      ],
-    ),
-  ),
-),
-          ],
         ),
       ),
     );
   }
 }
+
+
 
 
 
